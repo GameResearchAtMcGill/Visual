5dddd598eeeb8b454fc804b2e1bd5bf3fd2e4ef1
TontonTremblay <tonton.tremblay@gmail.com>
2014-07-31 13:40:34 -0400

    added files

---
+++ Stealth Design Tool/Assets/Scripts/RRT/RRTStart.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RRTStart : MapChild {
+
+	// Use this for initialization
+	new protected void Awake()
+	{
+		base.Awake(); 
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public override void MapChanged()
+	{
+		
+	}
+	public override void Validate()
+	{
+		position.y = 0;
+		
+		// Position clipping
+		if (position.x > 0.5f * map.dimensions.x) {
+			position.x = 0.5f * map.dimensions.x;
+		}
+		
+		if (position.z > 0.5f * map.dimensions.z) {
+			position.z = 0.5f * map.dimensions.z;
+		}
+		
+		if (position.x < -0.5f * map.dimensions.x) {
+			position.x = -0.5f * map.dimensions.x;
+		}
+		
+		if (position.z < -0.5f * map.dimensions.z) {
+			position.z = -0.5f * map.dimensions.z;
+		}
+	}
+}
